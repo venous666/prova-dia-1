cf409ab6e9d731b5d9f69c0c27bbfd7dc39455b4
venus <99412095+venous666@users.noreply.github.com>
2023-02-08 12:49:46 -0300

    Add files via upload

---
+++ questao3.js
@@ -0,0 +1,11 @@
+var valorbase = 50
+var minicurso = "s"
+var trad = "s"
+
+if(minicurso==s){
+    var novovalor =(valorbase + 20) 
+}
+else if(trad==s){
+    var novovalor =(valorbase + 10) 
+}
+console.log(novovalor)
